14aa0d032699c38fa3828c7e0c64d2c974f852e3
abdulrahmansbq <abdulrahmansbq@gmail.com>
2023-11-26 04:54:31 +0300

    Update Bill resource with its pages

--- app/Filament/Resources/BillResource/Pages/CreateBill.php
+++ app/Filament/Resources/BillResource/Pages/CreateBill.php
@@ -3,10 +3,21 @@
 namespace App\Filament\Resources\BillResource\Pages;
 
 use App\Filament\Resources\BillResource;
+use App\Models\Drug;
+use App\Models\Service;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateBill extends CreateRecord
 {
     protected static string $resource = BillResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $totalDrugsPrice = Drug::findMany($this->data['drugs'])->sum('price');
+        $totalServicesPrice = Service::findMany($this->data['services'])->sum('price');
+        $totalPrice = $totalDrugsPrice + $totalServicesPrice + $this->data['extra_charges'];
+        $data['total_price'] = $totalPrice;
+        return $data;
+    }
 }
--- app/Filament/Resources/BillResource/Pages/EditBill.php
+++ app/Filament/Resources/BillResource/Pages/EditBill.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Resources\BillResource\Pages;
 
 use App\Filament\Resources\BillResource;
+use App\Models\Drug;
+use App\Models\Service;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 
@@ -15,5 +17,14 @@ class EditBill extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        $totalDrugsPrice = Drug::findMany($this->data['drugs'])->sum('price');
+        $totalServicesPrice = Service::findMany($this->data['services'])->sum('price');
+        $totalPrice = $totalDrugsPrice + $totalServicesPrice + $this->data['extra_charges'];
+        $data['total_price'] = $totalPrice;
+        return $data;
     }
 }
