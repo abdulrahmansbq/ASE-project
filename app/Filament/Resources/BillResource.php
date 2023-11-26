a24c96e9d56e13a51f049223cad3d46c7ef74b99
abdulrahmansbq <abdulrahmansbq@gmail.com>
2023-11-26 05:46:31 +0300

    Update bill page icon

--- app/Filament/Resources/BillResource.php
+++ app/Filament/Resources/BillResource.php
@@ -23,7 +23,7 @@ class BillResource extends Resource
 {
     protected static ?string $model = Bill::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     public static function form(Form $form): Form
     {
